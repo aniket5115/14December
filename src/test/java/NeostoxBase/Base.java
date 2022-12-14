143791a15485c7814477c9c9bb263768edece8a1
Aniket <aniketthamke007@gmail.com>
2022-12-14 19:25:46 +0530

    Removed Spaces From Base

--- src/test/java/NeostoxBase/Base.java
+++ src/test/java/NeostoxBase/Base.java
@@ -11,11 +11,8 @@ public class Base {
 	    protected WebDriver driver;
 		public void Launchbrowser() throws InterruptedException
 		{
-			
-	      System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vinit\\Desktop\\1.Software Testing\\chromedriver.exe");
-			
-	      
-	        driver =new ChromeDriver();
+			 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vinit\\Desktop\\1.Software Testing\\chromedriver.exe");
+		    driver =new ChromeDriver();
 			driver.manage().window().maximize();
 			driver.get("https://neostox.com/sign-in");
 			Reporter.log("Launching Browser",true);
